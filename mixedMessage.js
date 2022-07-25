9c58a9e9b14870d6940ac131e2751ba154578bb3
Chayanon Tonsai <maxjunerd@gmail.com>
2022-07-26 04:59:26 +0700

    add inspirational messages

---
+++ mixedMessage.js
@@ -0,0 +1,34 @@
+messages = [
+    {
+        message: "Ambition is the path to success. Persistence is the vehicle you arrive in.",
+        author: "Bill Bradley"
+    },
+    {
+        message: "Give it your all. Whatever work you do, do it to the best of your ability.",
+        author: "Jerry Dorsman and Bob Davis"
+    },
+    {
+        message: "When ordinary people decide to step out and be part of something big, that’s when they become extraordinary.",
+        author: "Brett Harris, Do Hard Things"
+    },
+    {
+        message: "Inspiration responds to our attentiveness in various and sometimes unexpected ways.",
+        author: "Wayne W. Dyer"
+    },
+    {
+        message: "Words can inspire, thoughts can provoke, but only action truly brings you closer to your dreams.",
+        author: "Brad Sugars"
+    },
+    {
+        message: "Having a specific meaning and purpose in your life helps to encourage you towards living a fulfilling and inspired life.",
+        author: "Vic Johnson"
+    },
+    {
+        message: "When you see the world through serene eyes, you generate peace wherever you go.",
+        author: "Jerry Dorsman and Bob Davis"
+    },
+    {
+        message: "Embrace the present moment fully and with passion, because only through the present moment do we truly live.",
+        author: "Richard L. Haight"
+    }
+]
